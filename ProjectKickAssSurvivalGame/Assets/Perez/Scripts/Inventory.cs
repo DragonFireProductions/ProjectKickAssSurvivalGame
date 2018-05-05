a8e54c4c3995df7c356c2e3427b42b452d2c4cd0
Antonio Perez <aperez440@yahoo.com>
2018-05-05 16:54:19 -0400

    Date: 5/5/18
    Commit: Pushing my basic folders and the inventory and crafting scripts.
    
    By: Antonio Perez

---
+++ ProjectKickAssSurvivalGame/Assets/Perez/Scripts/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private int wood_resource;
+    private int stone_resource;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Wood_Resource")
+        {
+            wood_resource++;
+        }
+
+        if (gameObject.tag == "Stone_Resource")
+        {
+            stone_resource++;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (gameObject.tag == "Wood_Resource")
+        {
+            wood_resource++;
+        }
+
+        if (gameObject.tag == "Stone_Resource")
+        {
+            stone_resource++;
+        }
+    }
+
+    //public WoodResource()
+    //{
+    //    return wood_resource;
+    //}
+
+}
