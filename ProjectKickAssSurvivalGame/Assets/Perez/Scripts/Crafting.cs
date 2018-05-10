1578677e7d1366c195be6f7864378d5fbc08c9ad
Antonio Perez <aperez440@yahoo.com>
2018-05-10 13:13:48 -0400

    Date: 5/10/2018
    Commit: MAde changes to the inventory and crafting system.
    
    By:Antonio perez

--- ProjectKickAssSurvivalGame/Assets/Perez/Scripts/Crafting.cs
+++ ProjectKickAssSurvivalGame/Assets/Perez/Scripts/Crafting.cs
@@ -4,26 +4,73 @@ using UnityEngine;
 
 public class Crafting : MonoBehaviour
 {
-    [SerializeField] private bool CanCraft_WoodFence, CanCraft_StoneFence;
+    #region//Crafting completion criteria bools
 
+    [SerializeField] private bool CanCraft_BurlapWall, CanCraft_WoodFence, CanCraft_StoneFence, CanCraft_MetalFence, CanCraft_WoodTurret,
+    CanCraft_StoneTurret, CanCraft_MetalTurret, CanCraft_BurlapArmor, CanCraft_WoodArmor, CanCraft_StoneArmor, CanCraft_MetalArmor, CanCraft_SpikeTrap, CanCraft_BearTrap;
+
+    #endregion
+
+    #region//Crafting Game Objects
+
+    [SerializeField] private GameObject G_BurlapWall, G_WoodFence, G_StoneFence, G_MetalFence, G_WoodTurret, G_StoneTurret, G_MetalTurret,
+    G_BurlapArmor, G_WoodArmor, G_StoneArmor, G_MetalArmor, G_SpikeTrap, G_BearTrap;
+
+    #endregion
+
+    //Access to inventory script
     private Inventory Inv;
-
-    [SerializeField] private GameObject G_WoodFence, G_StoneFence;
 
     private void Start()
     {
         Inv = GetComponent<Inventory>();
 
+        //Crafting completion criteria bools set to false on start
+        CanCraft_BurlapWall = false;
         CanCraft_WoodFence = false;
         CanCraft_StoneFence = false;
+        CanCraft_MetalFence = false;
+        CanCraft_WoodTurret = false;
+        CanCraft_StoneTurret = false;
+        CanCraft_MetalTurret = false;
+        CanCraft_BurlapArmor = false;
+        CanCraft_WoodArmor = false;
+        CanCraft_StoneArmor = false;
+        CanCraft_MetalArmor = false;
+        CanCraft_SpikeTrap = false;
+        CanCraft_BearTrap = false;
     }
 
-    // Update is called once per frame
     void Update ()
     {
         WoodFence();
         StoneFence();
 	}
+
+    #region//Craftable Item Functions
+
+    private void BurlapWall()
+    {
+        if (Inv.sand_resource >= 1 && Inv.rope_resource >= 1)
+        {
+            CanCraft_BurlapWall = true;
+        }
+
+        if (CanCraft_BurlapWall == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(G_BurlapWall, transform.position + (transform.forward * 5), transform.rotation);
+                Inv.sand_resource--;
+                Inv.rope_resource--;
+            }
+        }
+
+        if (Inv.sand_resource <= 0 && Inv.rope_resource <= 0)
+        {
+            CanCraft_BurlapWall = false;
+        }
+    }
 
     private void WoodFence()
     {
@@ -36,7 +83,7 @@ public class Crafting : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                Instantiate(G_WoodFence, transform.forward + new Vector3(0,0,2), transform.rotation);
+                Instantiate(G_WoodFence, transform.position + (transform.forward * 5), transform.rotation);
                 Inv.wood_resource--;
             }
         }
@@ -58,7 +105,7 @@ public class Crafting : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Instantiate(G_StoneFence, transform.forward + new Vector3(0, 0, 2), transform.rotation);
+                Instantiate(G_StoneFence, transform.position + (transform.forward * 5), transform.rotation);
                 Inv.stone_resource--;
             }
         }
@@ -68,4 +115,116 @@ public class Crafting : MonoBehaviour
             CanCraft_StoneFence = false;
         }
     }
+
+    private void MetalFence()
+    {
+        if (Inv.metal_resource >= 1)
+        {
+            CanCraft_MetalFence = true;
+        }
+
+        if (CanCraft_MetalFence == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(G_MetalFence, transform.position + (transform.forward * 5), transform.rotation);
+                Inv.metal_resource--;
+            }
+        }
+
+        if (Inv.metal_resource <= 0)
+        {
+            CanCraft_MetalFence = false;
+        }
+    }
+
+    private void WoodTurret()
+    {
+        if (Inv.wood_resource >= 1)
+        {
+            CanCraft_WoodTurret = true;
+        }
+
+        if (CanCraft_WoodTurret == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(G_WoodTurret, transform.position + (transform.forward * 5), transform.rotation);
+                Inv.wood_resource--;
+            }
+        }
+
+        if (Inv.wood_resource <= 0)
+        {
+            CanCraft_WoodTurret = false;
+        }
+    }
+
+    private void StoneTurret()
+    {
+        if (Inv.stone_resource >= 1)
+        {
+            CanCraft_StoneTurret = true;
+        }
+
+        if (CanCraft_StoneTurret == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(G_StoneTurret, transform.position + (transform.forward * 5), transform.rotation);
+                Inv.stone_resource--;
+            }
+        }
+
+        if (Inv.stone_resource <= 0)
+        {
+            CanCraft_StoneTurret = false;
+        }
+    }
+
+    private void MetalTurret()
+    {
+        if (Inv.metal_resource >= 1)
+        {
+            CanCraft_MetalTurret = true;
+        }
+
+        if (CanCraft_MetalTurret == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(G_MetalTurret, transform.position + (transform.forward * 5), transform.rotation);
+                Inv.metal_resource--;
+            }
+        }
+
+        if (Inv.metal_resource <= 0)
+        {
+            CanCraft_MetalTurret = false;
+        }
+    }
+
+    private void BurlapArmor()
+    {
+        if (Inv.sand_resource >= 1 && Inv.rope_resource >= 1)
+        {
+            CanCraft_BurlapArmor = true;
+        }
+
+        if (CanCraft_BurlapArmor == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(G_BurlapArmor, transform.position + (transform.forward * 5), transform.rotation);
+                Inv.wood_resource--;
+            }
+        }
+
+        if (Inv.wood_resource <= 0)
+        {
+            CanCraft_WoodTurret = false;
+        }
+    }
+
+    #endregion
 }
