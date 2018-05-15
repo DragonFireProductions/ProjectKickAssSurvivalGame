de15ca76bf52f2972626d37e4cdaece5988752b4
Michael Buck II <MichaelBuck96@yahoo.com>
2018-05-14 20:47:13 -0400

    Date: 5/14/18 Commit: Had to fix some odd duplication withing the GameUI, started work on some procedural generation By: Michael J. Buck II

---
+++ ProjectKickAssSurvivalGame/Assets/Buck/Scripts/EXPERIMENTAL/ProceduralVoxelChunk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralVoxelChunk : MonoBehaviour
+{
+    public int xVoxels;
+    public int yVoxels;
+    public int Zvoxels;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
