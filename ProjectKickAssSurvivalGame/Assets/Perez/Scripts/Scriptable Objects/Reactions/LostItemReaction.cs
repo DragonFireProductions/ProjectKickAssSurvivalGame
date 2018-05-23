daaa15f4a60b481c3534c8ce2f3d828c171bdb13
Antonio Perez <aperez440@yahoo.com>
2018-05-22 20:04:20 -0400

    Date: 5/22/2018
    Commit: Crafting and tool belt system are done and inventory is further ahead.

---
+++ ProjectKickAssSurvivalGame/Assets/Perez/Scripts/Scriptable Objects/Reactions/LostItemReaction.cs
@@ -0,0 +1,19 @@
+﻿public class LostItemReaction : DelayedReaction
+{
+    public Item item;
+
+
+    private ToolBelt belt;
+
+
+    protected override void SpecificInit()
+    {
+        belt = FindObjectOfType<ToolBelt>();
+    }
+
+
+    protected override void ImmediateReaction()
+    {
+        belt.RemoveItem(item);
+    }
+}
